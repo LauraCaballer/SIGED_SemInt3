206e10822845d244a293dee754bb6242b205f77f
laura caballero <lauraalejandracp@ufps.edu.co>
2026-04-03 10:25:22 -0500

    Inicio

---
+++ Backend/siged/urls.py
@@ -0,0 +1,41 @@
+"""
+URL configuration for siged project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from api_auth.views import login_view, logout_view
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+
+    path('api/auth/login/', login_view),
+    path('api/auth/logout/', logout_view),
+
+    path('api/terceros/', include('terceros.urls')),
+    path('api/dominios_comunes/', include('dominios_comunes.urls')),
+    path('api/prendas/', include('prendas.urls')),
+    path('api/egreso_ingreso/', include('egreso_ingreso.urls')),
+    path('api/apartado_credito/', include('apartado_credito.urls')),
+    path('api/compra_venta/', include('compra_venta.urls')),
+
+
+    path('api/caja/', include('caja.urls')),
+]
