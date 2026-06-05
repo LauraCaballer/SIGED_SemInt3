2b83ba7532691e04796bc28b69751994f9956091
JuanPlay27 <juandavid_mr27@hotmail.com>
2026-06-04 23:01:10 -0500

    Cambios menores

--- Backend/dominios_comunes/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DominiosComunesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dominios_comunes'
--- Backend/terceros/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TercerosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'terceros'
