461cc5fc9d3fe44e31e6bde0151298a6ca4485b9
JuanPlay27 <juandavid_mr27@hotmail.com>
2026-06-04 23:48:25 -0500

    depuración total

--- Backend/prendas/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Prenda, TipoOro, TipoPrenda
-# Register your models here.
-admin.site.register(Prenda)
-admin.site.register(TipoPrenda)
-admin.site.register(TipoOro)
